836b9dbafdd85b219b53a16573aa8ab85ec6e584
icsd12058 <icsd12058@icsd.aegean.gr>
2018-02-21 14:34:22 +0200

    intent filter

--- app/src/main/java/com/icsd/game/thesis/Game13.java
+++ app/src/main/java/com/icsd/game/thesis/Game13.java
@@ -1,13 +1,25 @@
 package com.icsd.game.thesis;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.*;
+
+import java.io.IOException;
 
 public class Game13 extends AppCompatActivity {
-
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game13);
+        mp = MediaPlayer.create(this, R.raw.re);
+
+    }
+
+    public void playSound(View view) {
+
+
+        mp.start();
     }
 }
