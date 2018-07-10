c42d60c627b9c2fa21a23080d86c6146cdb9036e
Thelouras Konstantinos P <icsd12058@icsd.aegean.gr>
2018-07-10 16:50:43 +0300

    Game 13 END END END

--- app/src/main/java/com/icsd/game/thesis/game13/Game13.java
+++ app/src/main/java/com/icsd/game/thesis/game13/Game13.java
@@ -3,20 +3,28 @@ package com.icsd.game.thesis.game13;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.icsd.game.thesis.Menu;
 import com.icsd.game.thesis.R;
+import com.icsd.game.thesis.database.DatabaseHandler;
+import com.icsd.game.thesis.database.Session;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
-import static com.icsd.game.thesis.R.*;
+import static com.icsd.game.thesis.R.id;
+import static com.icsd.game.thesis.R.layout;
+import static com.icsd.game.thesis.R.raw;
 
 public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
 
@@ -50,15 +58,18 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
     private Runnable runChange1;
     private Runnable runChange2;
     private Runnable runCheck;
+    private Session curSession;
 
+    private DatabaseHandler dbHandler = new DatabaseHandler(this);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        curSession = new Session(Menu.testUser.getUsername(), 13);
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_game13);
+        setContentView(layout.game13_prototype);
         init();
-
+        curSession.setTimeStart(System.currentTimeMillis()/1000);
         try {
             playPattern();
         } catch (IOException e) {
@@ -70,34 +81,14 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
 
     public void onPause() {
         super.onPause();
-        la.pause();
-        re.pause();
-        mi.pause();
-        dof.pause();
-        handlerLa.removeCallbacks(run1);
-        handlerDof.removeCallbacks(run3);
-        handlerRe.removeCallbacks(run4);
-        change.removeCallbacks(runChange1);
-        change2.removeCallbacks(runChange2);
-        waitPlayer.removeCallbacks(runCheck);
+        //  killAll();
 
 
     }
 
     public void onStop() {
         super.onStop();
-        la.pause();
-        re.pause();
-        mi.pause();
-        dof.pause();
-        this.turn = 10;
-        handlerLa.removeCallbacks(run1);
-        handlerDof.removeCallbacks(run3);
-        handlerRe.removeCallbacks(run4);
-        change.removeCallbacks(runChange1);
-        change2.removeCallbacks(runChange2);
-        waitPlayer.removeCallbacks(runCheck);
-
+        //  killAll();
     }
 
 
@@ -108,9 +99,16 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
                 if (checkList.equals(patternList)) {
 
                     Log.e("Debug", "Correct");
+                    curSession.setScore(turn);
+                    curSession.setStage(turn);
                     turn++;
                     try {
-                        playPattern();
+                        if (turn == 5) {
+                            Toast.makeText(mContext, "YOU WON ", Toast.LENGTH_LONG).show();
+                            killAll();
+                        } else {
+                            playPattern();
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -119,6 +117,8 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
                 } else {
                     Log.e("Debug", "InCorrect");
                     try {
+                        curSession.setFails(curSession.getFails() + 1);
+                        Toast.makeText(mContext, "FAIL,TRY AGAIN ", Toast.LENGTH_LONG).show();
                         playPattern();
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -188,6 +188,7 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
             public void run() {
                 changeColor(noteNumnber);
                 checkPattern();
+
             }
         };
 
@@ -228,34 +229,34 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
         switch (turn) {
             case 0:
                 Log.e("Debug", " playpattern case 0");
-                handlerLa.postDelayed(run1, 800);
-                change.postDelayed(runChange1, 1200);
+                handlerLa.postDelayed(run1, 1100);
+                change.postDelayed(runChange1, 1500);
                 break;
             case 1:
-                handlerDof.postDelayed(run3, 800);
-                handlerRe.postDelayed(run2, 1200);
-                change2.postDelayed(runChange2, 1400);
+                handlerDof.postDelayed(run3, 1100);
+                handlerRe.postDelayed(run2, 1500);
+                change2.postDelayed(runChange2, 1700);
                 break;
             case 2:
-                handlerDof.postDelayed(run3, 800);
-                handlerRe.postDelayed(run4, 1200);
-                handlerLa.postDelayed(run1, 1400);
-                change2.postDelayed(runChange2, 1600);
+                handlerDof.postDelayed(run3, 1100);
+                handlerRe.postDelayed(run4, 1500);
+                handlerLa.postDelayed(run1, 1700);
+                change2.postDelayed(runChange2, 1900);
                 break;
             case 3:
-                handlerDof.postDelayed(run3, 800);
-                handlerRe.postDelayed(run4, 1200);
-                handlerLa.postDelayed(run1, 1400);
-                handlerMi.postDelayed(run2, 1600);
-                change2.postDelayed(runChange2, 1800);
+                handlerDof.postDelayed(run3, 1100);
+                handlerRe.postDelayed(run4, 1500);
+                handlerLa.postDelayed(run1, 1700);
+                handlerMi.postDelayed(run2, 1900);
+                change2.postDelayed(runChange2, 2100);
 
                 break;
             case 4:
-                handlerDof.postDelayed(run3, 800);
-                handlerRe.postDelayed(run4, 1200);
-                handlerLa.postDelayed(run1, 1400);
-                handlerMi.postDelayed(run2, 1600);
-                change2.postDelayed(runChange2, 1800);
+                handlerDof.postDelayed(run3, 1100);
+                handlerRe.postDelayed(run4, 1500);
+                handlerLa.postDelayed(run1, 1700);
+                handlerMi.postDelayed(run2, 1900);
+                change2.postDelayed(runChange2, 2100);
 
                 break;
 
@@ -341,6 +342,22 @@ public class Game13 extends AppCompatActivity implements MediaPlayer.OnPreparedL
 
     public void onPrepared(MediaPlayer player) {
 
+    }
+
+    public void killAll() {
+
+        la.pause();
+        re.pause();
+        mi.pause();
+        dof.pause();
+        handlerLa.removeCallbacks(run1);
+        handlerDof.removeCallbacks(run3);
+        handlerRe.removeCallbacks(run4);
+        change.removeCallbacks(runChange1);
+        change2.removeCallbacks(runChange2);
+        waitPlayer.removeCallbacks(runCheck);
+        dbHandler.addSessionToDB(this.curSession);
+        curSession.setTimeEnd(System.currentTimeMillis()/1000);
     }
 
     @Override
