cd8c07919da8aac4381357ae3eaee72bfa536b63
jimmaras7 <36486443+jimmaras7@users.noreply.github.com>
2018-07-12 20:55:29 +0300

    cc

--- app/src/main/java/com/icsd/game/thesis/Game12/Game12.java
+++ app/src/main/java/com/icsd/game/thesis/Game12/Game12.java
@@ -2,16 +2,18 @@ package com.icsd.game.thesis.Game12;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.Button;
 import com.icsd.game.thesis.R;
 
 
 public class Game12 extends AppCompatActivity{
+    private int winrate;
+    private Button checker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game12);
+     
 
     }
 }
