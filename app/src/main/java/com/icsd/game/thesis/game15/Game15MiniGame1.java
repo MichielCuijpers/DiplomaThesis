7fcd83efbfc4983f5c079a1c2cae1bb1852699e2
Thelouras Konstantinos P <icsd12058@icsd.aegean.gr>
2018-07-20 17:39:40 +0300

    game 15
    mini game 1 gui
    mini game 2 gui

--- app/src/main/java/com/icsd/game/thesis/game15/Game15MiniGame1.java
+++ app/src/main/java/com/icsd/game/thesis/game15/Game15MiniGame1.java
@@ -2,14 +2,53 @@ package com.icsd.game.thesis.game15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.icsd.game.thesis.R;
 
 public class Game15MiniGame1 extends AppCompatActivity {
+    TextView number1View;
+    TextView number2View;
+    TextView textView1;
+    TextView textView2;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game15_mini_game1);
+    }
+
+    private void initGui() {
+
+    }
+
+    private void initGameplay() {
+
+    }
+
+    private void initTurn() {
+
+    }
+
+    private void check() {
+
+    }
+
+    public void button1OnClick(View view) {
+    }
+
+    public void button2OnClick(View view) {
+    }
+
+    public void button3OnClick(View view) {
+    }
+
+    public void button4OnClick(View view) {
     }
 }
--- app/src/main/java/com/icsd/game/thesis/game15/Game15MiniGame2.java
+++ app/src/main/java/com/icsd/game/thesis/game15/Game15MiniGame2.java
@@ -2,6 +2,7 @@ package com.icsd.game.thesis.game15;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.icsd.game.thesis.R;
 
@@ -12,4 +13,52 @@ public class Game15MiniGame2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game15_mini_game2);
     }
+
+    public void number1OnClick(View view) {
+    }
+
+    public void number2OnClick(View view) {
+    }
+
+
+    public void number3OnClick(View view) {
+    }
+
+    public void number4OnClick(View view) {
+    }
+
+    public void number5OnClick(View view) {
+    }
+
+    public void number6OnClick(View view) {
+    }
+
+    public void number7OnClick(View view) {
+    }
+
+    public void number8OnClick(View view) {
+    }
+
+    public void number9OnClick(View view) {
+    }
+
+    public void number10OnClick(View view) {
+    }
+
+    public void number11OnClick(View view) {
+    }
+
+    public void number12OnClick(View view) {
+    }
+
+    public void number13OnClick(View view) {
+    }
+
+    public void number14OnClick(View view) {
+    }
+
+    public void number15OnClick(View view) {
+    }
+
+
 }
